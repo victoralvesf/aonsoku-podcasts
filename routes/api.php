8425f6f088f024138574e4a5254ed2601e80ba1c
Victor Alves <viictor.alvess12@gmail.com>
2024-12-28 15:18:04 -0300

    chore: remove hello world route

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,10 @@
 <?php
 
 use App\Http\Controllers\PodcastController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('podcast')->group(function () {
     Route::get('/', [PodcastController::class, 'index']);
     Route::post('/', [PodcastController::class, 'store']);
     Route::delete('/{id}', [PodcastController::class, 'destroy']);
-});
-
-Route::get('/hello', function (Request $request) {
-    return response()->json([
-        'message' => 'Hello World',
-        'user' => $request->user,
-        'tenant' => $request->tenant,
-    ]);
 });
