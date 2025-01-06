201b730801dfcb61007b590eb5f30ddb9b76edab
Victor Alves <viictor.alvess12@gmail.com>
2025-01-06 12:09:10 -0300

    fix: set correct name for PodcastIndexTest class

--- tests/Feature/PodcastIndexTest.php
+++ tests/Feature/PodcastIndexTest.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use PHPUnit\Framework\Attributes\Test;
 use Tests\TestCase;
 
-class PodcastTest extends TestCase
+class PodcastIndexTest extends TestCase
 {
     use RefreshDatabase;
 
