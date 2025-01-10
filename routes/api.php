6274a41cf25e1e30546d97d3e0abdbe63ee3c042
Victor Alves <viictor.alvess12@gmail.com>
2025-01-10 08:55:27 -0300

    chore: update route declarations

--- routes/api.php
+++ routes/api.php
@@ -4,15 +4,13 @@ use App\Http\Controllers\EpisodeController;
 use App\Http\Controllers\PodcastController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('podcasts')->group(function () {
-    Route::get('/search', [PodcastController::class, 'search'])->name('podcasts.search');
+Route::get('/podcasts/search', [PodcastController::class, 'search'])->name('podcasts.search');
 
-    Route::get('/', [PodcastController::class, 'index'])->name('podcasts.index');
-    Route::get('/{id}', [PodcastController::class, 'show'])->name('podcasts.show');
-    Route::post('/', [PodcastController::class, 'store'])->name('podcasts.store');
-    Route::delete('/{id}', [PodcastController::class, 'destroy'])->name('podcasts.destroy');
+Route::resource('podcasts', PodcastController::class)
+    ->only(['index', 'store', 'show', 'destroy']);
 
-    Route::get('/{id}/episodes/search', [EpisodeController::class, 'search'])->name('episodes.search');
-    Route::get('/episodes/latest', [EpisodeController::class, 'latest'])->name('episodes.latest');
-    Route::patch('/episodes/{id}/progress', [EpisodeController::class, 'progress'])->name('episodes.progress');
+Route::prefix('episodes')->name('episodes.')->group(function () {
+    Route::get('/podcast/{id}/search', [EpisodeController::class, 'search'])->name('search');
+    Route::get('/latest', [EpisodeController::class, 'latest'])->name('latest');
+    Route::patch('/{id}/progress', [EpisodeController::class, 'progress'])->name('progress');
 });
