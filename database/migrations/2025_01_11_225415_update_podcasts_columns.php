5e282bf6d2b85847ba91b87c813897c53e005677
Victor Alves <viictor.alvess12@gmail.com>
2025-01-11 20:03:12 -0300

    chore: update podcast and episode migrations

---
+++ database/migrations/2025_01_11_225415_update_podcasts_columns.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('podcasts', function (Blueprint $table) {
+            $table->text('description')->change();
+            $table->text('image_url')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('podcasts', function (Blueprint $table) {
+            $table->string('description')->change();
+            $table->string('image_url')->change();
+        });
+    }
+};
