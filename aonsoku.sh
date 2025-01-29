d0a07808c2b30674f20e60eaa78f62fbdde1b26b
Victor Alves <viictor.alvess12@gmail.com>
2025-01-29 09:28:14 -0300

    chore: config container timezone

--- aonsoku.sh
+++ aonsoku.sh
@@ -1,34 +1,54 @@
 #!/bin/bash
 
-# Check if an argument was provided
-if [ -z "$1" ]; then
+show_usage() {
     echo "Usage: $0 {start|queue|scheduler}"
     exit 1
+}
+
+set_timezone() {
+    if [ -n "$APP_TIMEZONE" ]; then
+        echo "Setting timezone to $APP_TIMEZONE..."
+        setup-timezone -z "$APP_TIMEZONE"
+    else
+        echo "APP_TIMEZONE not set. Using default timezone (UTC)."
+        setup-timezone -z UTC
+    fi
+}
+
+# Check if an argument was provided
+if [ -z "$1" ]; then
+    show_usage
 fi
 
 case "$1" in
     start)
+        set_timezone
         echo "Running migrations and caching configurations..."
         php artisan migrate --force
         php artisan config:cache
         php artisan route:cache
         echo "Starting php-fpm"
         php-fpm
+        echo "Service started successfully."
         ;;
 
     queue)
+        set_timezone
         echo "Starting the queue worker..."
         php artisan queue:work
+        echo "Queue worker started successfully."
         ;;
 
     scheduler)
+        set_timezone
         echo "Starting the scheduler..."
         crond -f &
         tail -f /var/log/schedule.log
+        echo "Scheduler started successfully."
         ;;
 
     *)
-        echo "Invalid command! Usage: $0 {start|queue|scheduler}"
-        exit 1
+        echo "Invalid command!"
+        show_usage
         ;;
 esac
