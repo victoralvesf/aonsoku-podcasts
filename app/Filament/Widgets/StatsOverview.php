b0c7bcaa519a40b76ca2bba576872847758361c1
Victor Alves <viictor.alvess12@gmail.com>
2025-08-17 17:04:48 -0300

    feat: add dashboard widgets

---
+++ app/Filament/Widgets/StatsOverview.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Admin;
+use App\Models\Episode;
+use App\Models\Podcast;
+use Filament\Support\Icons\Heroicon;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class StatsOverview extends StatsOverviewWidget
+{
+    protected static ?int $sort = 2;
+
+    protected ?string $pollingInterval = null;
+
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Podcasts count', Podcast::count())
+                ->icon(Heroicon::Microphone),
+            Stat::make('Episodes count', Episode::count())
+                ->icon(Heroicon::Signal),
+            Stat::make('Users count', Admin::count())
+                ->icon(Heroicon::UserGroup),
+        ];
+    }
+}
